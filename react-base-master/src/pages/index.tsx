a822441b4f4a389bfea5bec384b4054e2d67f2b8
Hieu-Noir <hieukebab2020@gmail.com>
2023-03-31 15:52:03 +0700

    update code

---
+++ react-base-master/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./index.less";
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 className={styles.title}>Welcome to My Page React sample CICD</h1>
+    </div>
+  );
+}
